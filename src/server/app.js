3746ae3d4b9e5f6781472c147fee30c94c81bd04
hussein88 <husseintaherian67@gmail.com>
2021-09-01 02:59:28 +0430

    Refactor app.js to serve index.html to user

--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 let app = express();
 app.set('PORT', 3300);
 app.get('/', (req,res) => {
-    return res.status(200).json({ 'hello': 'world'});
+    return res.status(200).sendFile(path.resolve(__dirname,'../','client','index.html'));
 });
 
 let server = http.createServer(app);
