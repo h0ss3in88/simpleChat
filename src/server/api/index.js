0a66ff3db9510127ac85f7d05f425d802a79f5f1
h0ss3in88 <husseinDeveloperx@protonmail.com>
2023-09-02 03:13:10 +0430

    accounts routers added to express app.

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,6 +1,8 @@
 const {pingRouter} = require('./lib/ping');
+const {accountsRouter} = require('./lib/accounts');
 
 const setupApi = ({app}) => {
     app.use('/api', pingRouter);
+    app.use('/api', accountsRouter);
 }
 module.exports = Object.assign({}, {setupApi});
